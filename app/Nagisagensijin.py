e1aa5735892efbd37a0fe47d360bd7723fbaf0c6
orangelinux <verneemix2.vlv@gmail.com>
2020-02-24 01:01:00 +0900

    adding text not found

---
+++ app/Nagisagensijin.py
@@ -0,0 +1,35 @@
+import nagisa
+from pykakasi import kakasi
+from flask import Flask
+from flask import Flask, request, render_template
+app = Flask(__name__)
+kakasi = kakasi()
+kakasi.setMode('J', 'K')
+@app.route('/')
+def index():
+    conv = kakasi.getConverter()
+    text = request.args.get('text')
+    if text:
+        words = nagisa.tagging(text)
+        print(words.words)
+        print(words.postags)
+        ary = []
+        for wwd,wd in enumerate(words.postags):
+             print(wd)
+             if wd=="名詞" or wd=="動詞" or wd=="助動詞":
+                 print("selected")
+                 ary.append(words.words[wwd])     
+             else:
+                 print("unselected")
+        strary1 = ','.join(ary)
+        strary = strary1.replace(',',' ')
+        ap = conv.do(strary)
+        kakasi.setMode('H','K')
+        conv = kakasi.getConverter()
+        return conv.do(ap)
+   else:
+       return "text not found."
+if __name__ == '__main__':
+    app.run()
+
+
